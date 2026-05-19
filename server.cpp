985d52f5389040e1ac3948d9f683f01c8a674644
AbhisekDas01 <dasabhisek668@gmail.com>
2026-05-19 18:08:18 +0000

    Implemented the TCP Server Demo

--- server.cpp
+++ server.cpp
@@ -1,20 +1,47 @@
-#include <stdio.h> //standard i/O header
+#include <iostream> //standard i/O header
 #include <sys/socket.h> 
 #include <netinet/in.h>
-#include <errno.h> //global variable the kernel use to store the last error
-#include <stdlib.h>
+#include <unistd.h> //used to handle file descriptors (read , write , close)
+#include <cerrno> //global variable the kernel use to store the last error
+#include <cstdlib>
+#include <cstring>
 
+
+
+static void msg(const char *msg) {
+    std::cerr << msg << std::endl;
+}
 
 static void die(const char *msg) {
     int err = errno;
-    fprintf(stderr, "[%d] %s\n", err, msg);
+    std::cerr << "[" << err << "] " << msg << std::endl; /*File Print Format: Unlike standard printf (which always prints to the screen), fprintf allows you to specify exactly where the text should be sent (to a file, a stream, or the screen).*/
     abort();
+}
+
+static void doSomething(int connfd) {
+    char rbuf[64] = {};
+
+    /*ssize_t = signed 64bit int(0 -> INT_MAX) + -1 (maximum network function returns -1 as a error code)*/
+    ssize_t n = read(connfd, rbuf , sizeof(rbuf)-1); //read return int value > 0 and -1 for errors
+    if(n < 0) {
+        msg("read() error");
+        return;
+    }
+
+    std::cout << "Client says: " << rbuf << std::endl;
+
+    const char* wbuf = "world\n";
+
+    write(connfd , wbuf , strlen(wbuf));
 }
 
 int main() {
 
             //socket(domain , conn-type , protocol)
     int fd = socket(AF_INET , SOCK_STREAM , 0);
+     if (fd < 0) {
+        die("socket()");
+    }
     
     /*
         1.AF_INET is for IPv4. Use AF_INET6 for IPv6 or dual-stack sockets.
@@ -25,6 +52,7 @@ int main() {
     //int setsockopt(int sockfd, int level, int optname,const void optval[.optlen], socklen_t optlen);
     setsockopt(fd ,SOL_SOCKET, SO_REUSEADDR , &val, sizeof(val));
 
+    //bind
     struct sockaddr_in addr = {}; //initiazlise a sockadder_in sturcture
     addr.sin_family = AF_INET;
     addr.sin_port = htons(1234); //htons converts the little endian representation to big endian 
@@ -42,7 +70,23 @@ int main() {
         die("listen()");
     }
 
+    std::cout << "Server is listening on port 1234..." << std::endl;
+
     //accept the incomming request
-    
+    while(true) {
+
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd , (struct sockaddr *)& client_addr , &addrlen);
+
+        if(connfd < 0) {
+            continue; //error 
+        }
+
+        doSomething(connfd);
+
+        close(connfd);
+    }
+    return 0;
 
 }
