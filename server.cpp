9b61893f29ff4824761a9d2bc84861a91a581fa6
AbhisekDas01 <dasabhisek668@gmail.com>
2026-05-19 16:48:24 +0000

    Added the socket ->initialization ->address binding -> listen

--- server.cpp
+++ server.cpp
@@ -1,5 +1,15 @@
 #include <stdio.h> //standard i/O header
 #include <sys/socket.h> 
+#include <netinet/in.h>
+#include <errno.h> //global variable the kernel use to store the last error
+#include <stdlib.h>
+
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
 
 int main() {
 
@@ -11,5 +21,28 @@ int main() {
         2.SOCK_STREAM is for TCP. Use SOCK_DGRAM for UDP.
         3.The 3rd argument is 0 and useless for our purposes.
      */
+    int val = 1;
+    //int setsockopt(int sockfd, int level, int optname,const void optval[.optlen], socklen_t optlen);
+    setsockopt(fd ,SOL_SOCKET, SO_REUSEADDR , &val, sizeof(val));
+
+    struct sockaddr_in addr = {}; //initiazlise a sockadder_in sturcture
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234); //htons converts the little endian representation to big endian 
+    addr.sin_addr.s_addr = htonl(0); // wildcard IP 0.0.0.0
+    
+    int rv = bind(fd , (const struct sockaddr *)&addr , sizeof(addr));
+
+    if(rv) {
+        die("bind()");
+    }
+
+    //listen
+    rv = listen(fd , SOMAXCONN);
+    if(rv) {
+        die("listen()");
+    }
+
+    //accept the incomming request
+    
 
 }
