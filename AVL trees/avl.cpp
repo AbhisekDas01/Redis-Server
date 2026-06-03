08bb3a3a92404da4e8147a4ebcdb5549fe367bcd
AbhisekDas01 <dasabhisek668@gmail.com>
2026-06-03 17:20:08 +0000

    Created the rotateLeft function

--- AVL trees/avl.cpp
+++ AVL trees/avl.cpp
@@ -16,3 +16,47 @@ static uint32_t avlHeight(AVLNode *node) {
 static void  avlUpdate(AVLNode *node) {
     node->height = 1 + max(avlHeight(node->left) , avlHeight(node->right));
 }
+
+
+//function to roate left (RR imbalance)
+/*
+    BEFORE
+       root -> 20                                               
+              /  \
+            10    40  <- newRoot 
+                 /  \
+      inner -> 30    50  
+                       \
+                       60  
+    AFTER
+                 40  <- newRoot
+                /  \
+       root-> 20    50 
+             /  \     \
+            10   30   60  
+                  ^
+                  |
+                inner
+*/
+static AVLNode *rotateLeft(AVLNode *root) {
+
+    AVLNode *parent = root->parent;
+    AVLNode *newRoot = root->right;
+    AVLNode *inner = newRoot->left;
+
+    root->right = inner; //connect the root to the internal node
+    if(inner) {
+        inner->parent = root;
+    }
+
+    newRoot->parent = parent; //change the parent
+
+    newRoot->left = root; //connect the newRoots left to the root //LL rotation
+    root->parent = newRoot;
+
+    //update the heights
+    avlUpdate(root);
+    avlUpdate(newRoot);
+    return newRoot;
+}
+
