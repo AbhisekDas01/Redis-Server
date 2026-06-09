37e111044dc7ed1f851efe95c43f260648e2faf3
AbhisekDas01 <dasabhisek668@gmail.com>
2026-06-09 15:52:33 +0000

    updated the common.h

--- common.h
+++ common.h
@@ -5,9 +5,8 @@
 
 
 // intrusive data structure
-#define container_of(ptr, type, member) ({                  \
-    const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-    (type *)( (char *)__mptr - offsetof(type, member) );})
+#define container_of(ptr, type, member) \
+    ((type *)((char *)(ptr) - offsetof(type, member)))
 
 
 //FNV hash function
